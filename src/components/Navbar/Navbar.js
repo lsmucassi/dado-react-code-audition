ec8162d669d8b7db24612c9f15aea7c323537fdf
lsmucassi <lindasmucassi@gmail.com>
2021-03-31 12:00:27 +0200

    dado git-commits: Navbar start

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    state = {  }
+    render() { 
+        return (
+            <nav className="navbarItems">
+                <h1 className="navbarLogo">CommitViewer</h1>
+                <div className="menuIcon">
+
+                </div>
+                <ul>
+                    <li>
+                        <a href=""></a>
+                    </li>
+                </ul>
+            </nav>
+        );
+    }
+}
+ 
+export default Navbar;
